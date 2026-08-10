b92bc624818bff78ee87ba35e36e2b4ed30e50ca
Ayaan ahmed <mohammedayaana588@gmail.com>
2026-08-10 19:02:53 +0530

    Main file

---
+++ jarvis/main.py
@@ -0,0 +1,104 @@
+import speech_recognition as sr
+import webbrowser
+import pyttsx3
+import musicLibrary
+from google import genai
+from dotenv import load_dotenv
+import os
+
+ # setup
+
+r = sr.Recognizer()
+ttsx = pyttsx3.init()
+
+
+def speak(text):
+    print("Jarvis:", text)
+
+    ttsx.say(text)
+    ttsx.runAndWait()
+
+load_dotenv()
+
+
+client = genai.Client(api_key=os.getenv("APIkey"))
+
+
+
+def process_command(c):
+    command = c.lower()
+
+ # Command to open websits 
+
+    if command == "open google":
+        webbrowser.open("https://google.com")
+        speak("opend google")
+
+    elif command == "open youtube":
+        webbrowser.open("https://youtube.com")
+        speak("opend youtube")
+
+    elif command == "open linkedin":
+        webbrowser.open("https://linkedin.com")
+        speak("opend linkedin")
+
+    elif command == "open instagram":
+        webbrowser.open("https://instagram.com")
+        speak("opend instagram")
+
+    elif command == "open github":
+            webbrowser.open("https://github.com")
+            speak("opend github")
+
+    elif command == "open yt music":
+            webbrowser.open("https://music.youtube.com/")
+            speak("opend yt music")
+
+    elif command == "open my github profile":
+                webbrowser.open("https://github.com/mohammedayaan-coder")
+                speak("opend your github profile")
+
+    else:
+        processai(command)
+
+def processai(command):
+
+    response = client.models.generate_content(
+        model="gemini-3.6-flash",
+        contents=command
+    )
+
+    answer = response.text
+    speak(answer)
+
+
+if __name__ == "__main__":
+
+    speak("Initializing Jarvis...")
+
+    while True:
+
+        try:
+            # Listen for wake word
+            with sr.Microphone() as source:
+                print("Listening...")
+                audio = r.listen(source)
+
+            word = r.recognize_google(audio)
+
+            if word.lower() == "jarvis":
+                speak("hello")
+                
+                # Listen for command
+                with sr.Microphone() as source:
+                    print("Jarvis active...")
+                    audio = r.listen(source, timeout=2,phrase_time_limit=2)
+
+                command = r.recognize_google(audio)
+
+                print("Command:", command)
+
+                process_command(command)
+
+        except Exception as e:
+            print("Error:", e)
